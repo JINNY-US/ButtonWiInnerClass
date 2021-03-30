635edb28dcd21bd7411380d548b6bb3ab2a132f7
{Kim-KyungJin} <{kyungjin2215@gmail.com}>
2021-03-30 10:39:07 +0900

    Inner class with this

--- app/src/main/java/com/example/buttonwiinnerclass/MainActivity.java
+++ app/src/main/java/com/example/buttonwiinnerclass/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.buttonwiinnerclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
         mtextview1 = findViewById(R.id.textView);
 
         mButton1.setOnClickListener(new MyOnclickListener(this));
+    }
+
+    private class MyOnclickListener implements View.OnClickListener {
+        MainActivity mainActivity;
+        public MyOnclickListener(MainActivity mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+
+        @Override
+        public  void onClick(View v) {
+            mainActivity.mtextview1.setText("YOu clicked 김경진's button!");
+        }
     }
 }
